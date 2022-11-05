5e6e99bbde299bc0e3d22cf06c9f08a1e43b399b
Lucas Matheus <matheusalexandre012@gmail.com>
2022-11-05 11:20:14 -0300

    organizando pastas

--- client/src/menuLateral.js
+++ client/src/menuLateral.js
@@ -1,11 +1,3 @@
-import express from "express";
-const app = express();
-
-app.listen(3000, () => {  
-    console.log("Server running on 3000");
-});
-
-
 // Seletor de Menu lateral 
 const list = document.querySelectorAll('.list');
 
