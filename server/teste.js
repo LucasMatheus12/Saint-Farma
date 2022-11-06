a0d8270524c3347d33a79c2dd3820b80afa502cb
Lucas Matheus <matheusalexandre012@gmail.com>
2022-11-06 09:40:44 -0300

    testando rotas

---
+++ server/teste.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json())
+
+const port = 3000; 
+
+app.get("/teste", (req, res) => res
+.status(200)
+.send({mensagem: "Bem vindo ao teste!"
+}))
+
+app.listen(port, () =>{
+    console.log(`Servidor rodadndo na porta ${port}`)
+})
+
+module.exports = app; 
