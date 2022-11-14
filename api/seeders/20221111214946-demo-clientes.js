c0646a026298102a8a9dad59ba367c9b20cef913
Lucas Matheus <matheusalexandre012@gmail.com>
2022-11-14 17:09:16 -0300

    teste

--- api/seeders/20221111214946-demo-clientes.js
+++ api/seeders/20221111214946-demo-clientes.js
@@ -1,15 +1,39 @@
-// 'use strict';
+'use strict';
 
-// module.exports = {
-//   up: (queryInterface, Sequelize) => {
+module.exports = {
+  up: (queryInterface, Sequelize) => {
 
-//     return queryInterface.bulkInsert('Clientes', 
-//     [  
-//   ], {});
+    return queryInterface.bulkInsert('Clientes', 
+    [
+      {
+        nomes: 'Alisson castro',
+        cpf: '741852963',
+        telefone: '88969587412', 
+        rua: 'josefa', 
+        bairro: 'ze', 
+        numeroCasa: '88',
+        cidade:'patu', 
+        referencia:'vizinho ao chaves',
+				createdAt: new Date(),
+				updatedAt: new Date()
+    },
+    {
+      nomes: 'alice',
+      cpf: '741852963',
+      telefone: '8896658512', 
+      rua: 'josefa', 
+      bairro: 'ze', 
+      numeroCasa: '88',
+      cidade:'patu', 
+      referencia:'vizinho ao chaves',
+      createdAt: new Date(),
+      updatedAt: new Date()
+  }
+  ], {});
 
-//   },
+  },
 
-//   down: (queryInterface, Sequelize) => {
-//     return queryInterface.bulkDelete('Clientes', null, {});
-//   }
-// };
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Clientes', null, {});
+  }
+};
